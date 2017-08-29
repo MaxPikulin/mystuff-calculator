7c46df3512bdb69725802a640079d4bf786fd46b
MaxPikulin <trurl1000@gmail.com>
2017-08-29 20:47:32 +0100

    First "working" version :)

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const table = document.querySelector('table');
 const display = document.querySelector('.display');
+const history = document.querySelector('.history');
 
-table.addEventListener('click', (e) => {
-    console.log(e.target.textContent);
-    display.textContent += e.target.textContent;
-});
+let aNum, bNum, oper, done = false;
+const operators = {
+    "+": function() { return aNum + bNum },
+    "-": function() { return aNum - bNum },
+    "*": function() { return aNum * bNum },
+    "/": function() { return aNum / bNum }
+}
+
+function operation(a, b) {
+    console.log()
+}
+
+function handler(e) {
+    let symbol = e.target.textContent;
+    if (/[-+*/]/.test(symbol)) {
+        aNum = parseInt(display.textContent);
+        oper = symbol;
+        history.textContent = aNum + ' ' + oper;
+        display.textContent = '0';
+        return;
+    }
+    if (symbol == '=') {
+        bNum = parseInt(display.textContent);
+        let result = operators[oper]();
+        display.textContent = result;
+        history.textContent += ' ' + bNum + ' = ' + result;
+        done = true;
+        return;
+    }
+
+    display.textContent == 0 || done ? display.textContent = symbol : display.textContent += symbol;
+}
+
+table.addEventListener('click', handler);
