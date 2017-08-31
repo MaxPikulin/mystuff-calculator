21d3c81bd8f12ca9bb2ecca9f019a588a0c3d723
MaxPikulin <trurl1000@gmail.com>
2017-08-31 15:14:36 +0100

    Decent progress, spaghetti yum.

--- script.js
+++ script.js
@@ -2,37 +2,79 @@ const table = document.querySelector('table');
 const display = document.querySelector('.display');
 const history = document.querySelector('.history');
 
-let aNum, bNum, oper, done = false;
-const operators = {
-    "+": function() { return aNum + bNum },
-    "-": function() { return aNum - bNum },
-    "*": function() { return aNum * bNum },
-    "/": function() { return aNum / bNum }
-}
+let currNum, prevNum, operator, done = false;
+const operation = {
+    "+": function() { return +prevNum + +currNum },
+    "-": function() { return +prevNum - +currNum },
+    "*": function() { return +prevNum * +currNum },
+    "/": function() { return +prevNum / +currNum },
+    "%": function() { return +prevNum % +currNum }
+};
 
-function operation(a, b) {
-    console.log()
-}
+function calcHandler(e) {
+    if (e.target.localName != 'td') return;
+    let symbol = e.target.innerText;
 
-function handler(e) {
-    let symbol = e.target.textContent;
-    if (/[-+*/]/.test(symbol)) {
-        aNum = parseInt(display.textContent);
-        oper = symbol;
-        history.textContent = aNum + ' ' + oper;
-        display.textContent = '0';
-        return;
+    switch (symbol) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            currNum = (currNum || '') + symbol;
+            break;
+        case '.':
+            if (currNum != undefined && currNum.includes('.')) return;
+            currNum = (currNum ? currNum : '0') + symbol;
+            break;
+        case 'C':
+            prevNum = currNum = undefined;
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+            if (prevNum != undefined) {
+                currNum = operation[operator]();
+                display.textContent = currNum;
+            }
+            operator = symbol;
+            prevNum = currNum;
+            currNum = undefined;
+            return;
+
+
+        case '=':
+            if (currNum == undefined) return;
+            display.textContent = currNum = operation[operator]();
+            operator = undefined;
+            prevNum = undefined;
+            return;
     }
-    if (symbol == '=') {
-        bNum = parseInt(display.textContent);
-        let result = operators[oper]();
-        display.textContent = result;
-        history.textContent += ' ' + bNum + ' = ' + result;
-        done = true;
-        return;
-    }
+    display.textContent = currNum || '0';
+    // if (/[-+*/]/.test(symbol)) {
+    //     aNum = parseInt(display.textContent);
+    //     oper = symbol;
+    //     history.textContent = aNum + ' ' + oper;
+    //     display.textContent = '0';
+    //     return;
+    // }
+    // if (symbol == '=') {
+    //     currNum = parseInt(display.textContent);
+    //     let result = operators[oper]();
+    //     display.textContent = result;
+    //     history.textContent += ' ' + currNum + ' = ' + result;
+    //     done = true;
+    //     return;
+    // }
 
-    display.textContent == 0 || done ? (display.textContent = symbol, done = false) : display.textContent += symbol;
+    // display.textContent == 0 || done ? (display.textContent = symbol, done = false) : display.textContent += symbol;
 }
 
-table.addEventListener('click', handler);
+table.addEventListener('click', calcHandler);
